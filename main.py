60673a5b664ed577fc2826202f809716b2f7609f
prova <ester@eceland.it>
2024-10-04 09:19:10 +0200

    Trying new stuff in python, just a button.
    Signed-off-by: Ester Gherbezza <ester.gherbez@gmail.coma>

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import sys
+
+from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
+# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
-
-    # noinspection PyUnresolvedReferences
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("My App")
         button = QPushButton("Press Me!")
-        button.clicked.connect(self.the_button_was_clicked)
-        self.setCentralWidget(button)
 
-    @staticmethod
-    def the_button_was_clicked():
-        print("Clicked!")
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
 
 
 app = QApplication(sys.argv)
