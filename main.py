0ebc7d87fabb7c3809727602e3603103f74b54e1
Ester Gherbezza <ester.gherbezza@gmail.com>
2024-10-04 10:08:56 +0200

    Basics, - toggle button - updating text with input lbl
    Signed-off-by: Ester Gherbezza <ester.gherbezza@gmail.com>

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget
 
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.button_is_checked = True
 
         self.setWindowTitle("My App")
-        button = QPushButton("Press Me")
+        self.button = QPushButton("Press Me")
+
+        self.button.setCheckable(True)  # makes a toggle
+        self.button.clicked.connect(self.the_button_was_clicked)
+        self.button.clicked.connect(self.the_button_was_toggled)
+        self.button.setChecked(self.button_is_checked)  # sets value of toggle
 
         # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        self.button.setText("clicked")
+
+    def the_button_was_toggled(self, checked):
+        self.button_is_checked = checked
+        print(checked)
 
 
 app = QApplication(sys.argv)
@@ -22,3 +35,35 @@ window = MainWindow()
 window.show()
 
 app.exec()
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(container)
+
+
+app2 = QApplication(sys.argv)
+
+window = Window()
+window.show()
+
+app2.exec()
+
+
